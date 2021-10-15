909418764dffe2a29e8abc292a8f288b0bdabf9e
Build-coder <59242259+Build-coder@users.noreply.github.com>
2021-10-14 23:46:20 -0400

    Implemented tests

--- src/test/java/AccountServiceTests.java
+++ src/test/java/AccountServiceTests.java
@@ -1,19 +1,19 @@
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+import java.util.List;
+import java.util.ArrayList;
 
 import java.sql.Connection;
 import java.sql.PreparedStatement;
-import java.sql.ResultSet;
 import java.sql.SQLException;
 
-import com.revature.daos.AccountDAOImpl;
 import com.revature.models.*;
 import com.revature.services.AccountService;
 import com.revature.utils.ConnectionUtil;
 
-import org.junit.jupiter.api.Assertions.*;
-import org.junit.jupiter.api.AfterAll;
-import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
@@ -22,18 +22,21 @@ import org.slf4j.LoggerFactory;
 
 public class AccountServiceTests {
     
-    public static AccountService accountServ; 
     public static Logger log = LoggerFactory.getLogger(AccountServiceTests.class);
 
-    public static String account_id = "12345";
-    public static float balance = 0;
-    public static boolean active = false;
+    public static AccountService accountServ; 
+    public static String account_id;
+    public static float balance;
+    public static boolean active;
     public static String customer_id;
-    public static Customer c = null;
+    public static Customer c;
+    public static Account a;
+    public static float amount;
+    public static List<Account> accounts;
 
     @BeforeAll
-    public static void setAccountServ(){
-        log.info("In setAccountServ()");
+    public static void createObjects(){
+        log.info("In createObjects()");
         accountServ = new AccountService();
     }
 
@@ -41,81 +44,99 @@ public class AccountServiceTests {
     public void setVars(){
         log.info("In setVars()");
         account_id = "12345";
-        balance = 0;
+        balance = 50;
         active = false;
         customer_id = "10101";
-        c.setId("10101");
-        c.setName("phil");
-        c.setPassword("dog");
+        amount = 25;
+        accounts = new ArrayList<>();
+        c = new Customer("10101","phil","dog");
+
+        // control object - exists only in this file
+        a = new Account(account_id, balance, active, c); 
+
+        // test object - writes to database
+        accountServ.createAccount(account_id, balance, active, c);
+
+        // both control and test are init with same values
+        // they share the same id so db should recognize them as 
+        // the same
     }
 
     @Test
-	public void testCreateAccount() {
+    public void testCreateAccount(){
         log.info("In testCreateAccount()");
-        
-        // object existing only in this scope
-        Account a = new Account(account_id, balance, active, c);
 
-        // object being written to the database
+        /**
+         * but is 'a' from the db or local?
+         */
         accountServ.createAccount(account_id, balance, active, c);
-        
-        try(Connection conn = ConnectionUtil.getConnection()){ //try-with-resources 
-			String sql = "SELECT * FROM accounts WHERE account_id = ?;";
-			
-			PreparedStatement statement = conn.prepareStatement(sql);
-			
-			statement.setString(1, account_id);
-			
-			ResultSet result = statement.executeQuery();
-			
-			Account account = new Account();
-			
-			//ResultSets have a cursor (similar to Scanner or other I/O classes) 
-            //that can be used with a while loop to iterate through all the data. 
-			
-			if(result.next()) {
-				
-				account.setAccountID(result.getString("account_id"));
-				account.setBalance(result.getFloat("account_balance"));
-				account.setActive(result.getBoolean("account_active"));
-				account.setCustomerID(result.getString("customer_id"));
-			}
-			
-            // assert that object in scope is equal to object in database
-			assertEquals(a, account);
-			
-		}catch (SQLException e) {
-			e.printStackTrace();
-		}
-	}
+
+        assertEquals(account_id, a.getAccountID());
+    }
  
      @Test
-	 public void add() {
-        log.info("In add()");
+     public void testAdd() {
+        log.info("In testAdd()");
 
-	 	assertEquals(1,1);
-	 }
+		accountServ.add(a, amount);
+        /**
+         * 'a' must be written to db b/c otherwise a.getBalance()
+         * would equal balance, not balance + amount 
+         */
+        assertEquals((balance+amount), a.getBalance());
+	}
 
-    // @Test
-	// public void subtract(Account account, int amount) {
-	// 	account.setBalance(account.getBalance() - amount);
-	// }
+    @Test
+	public void testSubtract() {
+        log.info("In testSubtract()");
 
-    // @Test
-	// public void updateAccount(Account x){
-	// 	// if account == null, init account
-		
-	// }
+		accountServ.subtract(a, amount);
+        assertEquals((balance-amount), a.getBalance());
+	}
+    
+	@Test
+	public void testGetAllAccounts(){
+        log.info("In testGetAllAccounts()");
 
-    // @AfterEach
-    // public void clearResults(){
+	
+        // remove all accounts
+        try(Connection conn = ConnectionUtil.getConnection()){
 
-    // }
+            String sql = "TRUNCATE TABLE accounts CASCADE;";
+                
+            PreparedStatement statement = conn.prepareStatement(sql);
+        
+            statement.execute();
+    
+        }catch(SQLException e) {
+            e.printStackTrace();
+        }
 
-    // @AfterAll
-    // public static void clearAccountServ(){
-    //     accountServ = null;
-    //     log.info("in clearAccountServ");
-    // }
+        // add 3 new accounts (they don't seem to be added for some reason)
+        accountServ.createAccount("12345", balance, active, c);
+        accountServ.createAccount("54321", balance, active, c);
+        accountServ.createAccount("10101", balance, active, c);
 
+        accounts = accountServ.getAllAccounts();
+
+        // assert that all 3 accounts were retrieved by getAllAccounts()
+        assertEquals(3,accounts.size());
+	}
+
+    @Test
+	public void testRemove(){
+        log.info("In testRemove()");
+
+        // doesn't seem to be removing for some reason
+		accountServ.remove(a);
+        assertNotEquals(account_id, a.getAccountID());
+        assertThrows(NullPointerException.class, () -> a.getAccountID());
+	}
+
+    @Test
+	public void testUpdateAccountStatus(){
+        log.info("In testUpdateAccountStatus");
+
+		assertEquals(active, a.isActive());
+	}
 }
